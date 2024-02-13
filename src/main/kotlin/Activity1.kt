dbb886919ae3b3c949cca140e5df28f380c5bc16
Activity1 <diocsonleannzaira0@gmail.com>
2024-02-13 19:18:17 +0800

    Initial Commit

---
+++ src/main/kotlin/Activity1.kt
@@ -0,0 +1,23 @@
+package org.example
+
+fun main() {
+
+    var noOfGroup = 1..4
+
+    for(group in noOfGroup) {
+
+    print("Enter your firstname: ")
+    var fname = readLine()
+
+        print("Enter your middle initial: ")
+        var mi = readLine()
+
+        print("Enter your lastname: ")
+        var lname = readLine()
+
+    print("Enter your age: ")
+    var age = readLine()
+
+        println("Your name is $fname $mi. $lname and your age is $age")
+    }
+}
